b9545dad3d912216454da41375c5663c2347fc6e
CH Barbosa <carloshenriquefb@gmail.com>
2018-01-13 17:12:45 -0300

    restaurant list animation

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { animate, trigger, state, style, transition } from '@angular/animations';
 
 import { Restaurant } from './restaurant.model';
 
 @Component({
   selector: 'mt-restaurant',
   templateUrl: './restaurant.component.html',
-  styleUrls: ['./restaurant.component.css']
+  styleUrls: ['./restaurant.component.css'],
+  animations: [
+    trigger('restaurantAppeared',[
+      state('ready', style({opacity:1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translate(-30px, -10px)'}),
+        animate('300ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
+
+  restaurantState = 'ready';
 
   @Input() restaurant: Restaurant;
 
